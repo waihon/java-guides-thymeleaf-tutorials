d2350d2217272cf044e45056707c762bbffa5614
Waihon Yew <yewwaihon@gmail.com>
2025-02-23 18:24:17 +0800

    Initial version generated using Spring Initializr

---
+++ src/main/java/com/waihon/javaguides/thymeleaf/tutorials/TutorialsApplication.java
@@ -0,0 +1,13 @@
+package com.waihon.javaguides.thymeleaf.tutorials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorialsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/waihon/javaguides/thymeleaf/tutorials/TutorialsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.waihon.javaguides.thymeleaf.tutorials;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorialsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
