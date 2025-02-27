162a364f3a238e486d97e093dba72096a757a3df
Waihon Yew <yewwaihon@gmail.com>
2025-02-27 23:31:59 +0800

    Handler to process submitted form

--- src/main/java/com/waihon/javaguides/thymeleaf/tutorials/controller/FormController.java
+++ src/main/java/com/waihon/javaguides/thymeleaf/tutorials/controller/FormController.java
@@ -4,6 +4,8 @@ import com.waihon.javaguides.thymeleaf.tutorials.model.UserForm;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Arrays;
 import java.util.List;
@@ -21,6 +23,14 @@ public class FormController {
         model.addAttribute("professionList", professionList);
 
         return "register-form";
+    }
+
+    @PostMapping("register/save")
+    public String submitForm(Model model,
+                             @ModelAttribute("userForm") UserForm userForm) {
+        model.addAttribute("userForm", userForm);
+
+        return "register-success";
     }
 
 }
